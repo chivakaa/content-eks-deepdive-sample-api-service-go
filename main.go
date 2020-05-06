65e03d7eb33171473996ab65655a3f5b0e5f7eca
chivakaa <chivakaa@hotmail.com>
2020-05-06 15:14:42 +0200

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hola Dan, version 2 del fichero"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
